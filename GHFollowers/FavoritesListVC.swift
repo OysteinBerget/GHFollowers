484e44208b69de87ee8fab536396c0c06e539c49
Øystein Berget <oystein.berget@gmail.com>
2020-01-08 11:55:09 +0100

    CH-6 Added Search Screen

--- GHFollowers/FavoritesListVC.swift
+++ GHFollowers/FavoritesListVC.swift
@@ -9,11 +9,15 @@
 import UIKit
 
 class FavoritesListVC: UIViewController {
+    
+    var username: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
     // MARK: - Navigation
