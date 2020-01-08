ae1274a5feff9cad21085575aa1af2865026b625
Øystein Berget <oystein.berget@gmail.com>
2020-01-08 09:34:46 +0100

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Øystein Berget on 08/01/2020.
+//  Copyright © 2020 Øystein Berget. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
