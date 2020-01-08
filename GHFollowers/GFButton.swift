bba137ae93339e7a06cdafe63a40cc3551ab521b
Øystein Berget <oystein.berget@gmail.com>
2020-01-08 11:27:53 +0100

    CH5 Added custom GFButton and GFTextField

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,38 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Øystein Berget on 08/01/2020.
+//  Copyright © 2020 Øystein Berget. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // Custom Code
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
