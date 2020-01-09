2a81764e4835cb84dab75d37f4d781229903baa6
Øystein Berget <oystein.berget@gmail.com>
2020-01-09 21:54:34 +0100

    CH-19 ARC, Memory Leaks, and Capture Lists [weak self] and moved creation of CollectionView Flow Layout to UIHelper

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -43,41 +43,11 @@ class FollowerListVC: UIViewController {
     
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
         
-    }
-    
-    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minimumItemSpacing: CGFloat = 10
-        let avaiableWidth =  width - (padding * 2) - (minimumItemSpacing * 2)
-        let itemWidth = avaiableWidth / 3
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-    
-        return flowLayout
-    }
-    
-    
-    func getFollowers() {
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-            switch result {
-            case .success(let followers):
-                print("Followers.count = \(followers.count)")
-                self.followers = followers
-                self.updateData()
-                
-            case .failure(let error):
-                self.presentGFAlertOnMainTread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
-                
-            }
-        }
     }
     
     
@@ -90,6 +60,23 @@ class FollowerListVC: UIViewController {
             return cell
         })
         
+    }
+    
+    
+    func getFollowers() {
+        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let followers):
+                print("Followers.count = \(followers.count)")
+                self.followers = followers
+                self.updateData()
+                
+            case .failure(let error):
+                self.presentGFAlertOnMainTread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
+                
+            }
+        }
     }
     
     
---
+++ GHFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,26 @@
+//
+//  UIHelper.swift
+//  GHFollowers
+//
+//  Created by Øystein Berget on 09/01/2020.
+//  Copyright © 2020 Øystein Berget. All rights reserved.
+//
+
+import UIKit
+
+struct UIHelper {
+    static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let avaiableWidth =  width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = avaiableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+    
+        return flowLayout
+    }
+    
+}
