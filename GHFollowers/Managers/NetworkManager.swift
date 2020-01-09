4c1646d0aef4a7b579b99e6f0c66f989c277a809
Øystein Berget <oystein.berget@gmail.com>
2020-01-09 23:29:49 +0100

    CH-20 Added Caching of Downloaded Avatar Images;

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -6,11 +6,12 @@
 //  Copyright © 2020 Øystein Berget. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com"
+    private let baseURL = "https://api.github.com"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
