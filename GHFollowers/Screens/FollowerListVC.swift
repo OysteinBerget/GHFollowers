474acdddc072a88339c9c2a0ca7cb7ccd7848dfd
Øystein Berget <oystein.berget@gmail.com>
2020-01-09 16:09:14 +0100

    CH-18 Configured UICOllectionView Diffable Data Source

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -10,14 +10,23 @@ import UIKit
 
 class FollowerListVC: UIViewController {
     
+    enum Section {
+        case main
+    }
+    
     var username: String!
+    var followers: [Follower] = []
+    
     var collectionView: UICollectionView!
+    var datasource: UICollectionViewDiffableDataSource<Section, Follower>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
         configureCollectionView()
+        configureDataSource()
         getFollowers()
+        
     }
     
     
@@ -61,7 +70,8 @@ class FollowerListVC: UIViewController {
             switch result {
             case .success(let followers):
                 print("Followers.count = \(followers.count)")
-                print(followers)
+                self.followers = followers
+                self.updateData()
                 
             case .failure(let error):
                 self.presentGFAlertOnMainTread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
@@ -70,6 +80,28 @@ class FollowerListVC: UIViewController {
         }
     }
     
+    
+    func configureDataSource() {
+        datasource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, follower) -> UICollectionViewCell? in
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            
+            return cell
+        })
+        
+    }
+    
+    
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async {
+            self.datasource.apply(snapshot, animatingDifferences: true)
+        }
+        
+    }
     
     // MARK: - Navigation
     
