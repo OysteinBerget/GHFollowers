45e50e4d91ccbc898a15a0b6c7349ef89977ac55
Øystein Berget <oystein.berget@gmail.com>
2020-01-10 15:12:59 +0100

    CH-22 Added Loading View Activity indicator

--- GHFollowers/Extentions/UIViewController-Ext.swift
+++ GHFollowers/Extentions/UIViewController-Ext.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+// View used to show loading view
+fileprivate var containerView: UIView!
+
 extension UIViewController {
     func presentGFAlertOnMainTread(title: String, message: String, buttonTitle: String) {
         DispatchQueue.main.async {
@@ -16,6 +19,38 @@ extension UIViewController {
             alertVC.modalTransitionStyle = .crossDissolve
             
             self.present(alertVC, animated: true)
+        }
+    }
+    
+    
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor   = .systemBackground
+        containerView.alpha             = 0
+        
+        UIView.animate(withDuration: 0.25) { containerView.alpha = 0.8 }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        activityIndicator.startAnimating()
+        
+    }
+    
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -67,8 +67,11 @@ class FollowerListVC: UIViewController {
     
     
     func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
+            
             switch result {
             case .success(let followers):
                 print("Followers.count = \(followers.count)")
