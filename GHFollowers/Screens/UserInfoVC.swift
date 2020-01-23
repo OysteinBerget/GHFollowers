160e7a502a715f0e5628afdc3ea84dce3051edfa
Øystein Berget <oystein.berget@gmail.com>
2020-01-23 10:40:25 +0100

    CH-27 Added UserInfoHaedVC

--- GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class UserInfoVC: UIViewController {
     
+    // MARK: - Properties
+    let headerView = UIView()
+    
     var username: String!
-    var user: User!
+   
 
+    // MARK: - LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -20,15 +24,18 @@ class UserInfoVC: UIViewController {
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
         
+        layoutUI()
+        
         guard let username = username else { return }
-        title = username
         
         NetworkManager.shared.getUserInfo(for: username, completed: { [weak self] (result) in
             guard let self = self else { return }
             
             switch result {
             case .success(let user):
-                self.user = user
+                DispatchQueue.main.async {
+                    self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
+                }
                 print(user)
                 
             case .failure(let error):
@@ -38,7 +45,33 @@ class UserInfoVC: UIViewController {
         
     }
     
-
+    
+    // MARK: - Configuration Methods
+    func layoutUI() {
+        view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180)
+        ])
+    }
+    
+    
+    func add(childVC: UIViewController, to containerView: UIView) {
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
+        
+    }
+    
+    
+    
+    
+    // MARK: - Navigation Methods
     @objc func dismissVC() {
         dismiss(animated: true)
     }
---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Øystein Berget on 23/01/2020.
+//  Copyright © 2020 Øystein Berget. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
