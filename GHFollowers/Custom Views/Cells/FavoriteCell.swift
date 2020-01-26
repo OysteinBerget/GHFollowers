37cd59e55a598a4696da8651dc5f7d81bbf88b53
Øystein Berget <oystein.berget@gmail.com>
2020-01-26 15:57:34 +0100

    CH-38 Created FavorireCell view for the Favorites View Controller table view.

---
+++ GHFollowers/Custom Views/Cells/FavoriteCell.swift
@@ -0,0 +1,58 @@
+//
+//  FavoriteCell.swift
+//  GHFollowers
+//
+//  Created by Øystein Berget on 26/01/2020.
+//  Copyright © 2020 Øystein Berget. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+
+    // MARK: - Properties
+    static let resuseID = "FavoriteCell"
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLable   = GFTitleLabel(textAlignment: .left, fontSize: 26)
+    
+    
+    // MARK: - Initialize Methods
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Configuration Methods
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLable)
+        
+        accessoryType           = .detailDisclosureButton
+        let padding: CGFloat    = 12
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            
+            usernameLable.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            usernameLable.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
+            usernameLable.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            usernameLable.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+    
+    func set(favorite: Follower) {
+        usernameLable.text = favorite.login
+        avatarImageView.downloadImage(from: favorite.avatarUrl)
+    }
+    
+}
