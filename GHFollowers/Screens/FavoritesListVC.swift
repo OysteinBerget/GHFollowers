1394e790c252e0ac7c175083ab3f89e938326418
Øystein Berget <oystein.berget@gmail.com>
2020-01-26 15:27:16 +0100

    CH-37 Added Add user to Favorites in FollowersListVC

--- GHFollowers/Screens/FavoritesListVC.swift
+++ GHFollowers/Screens/FavoritesListVC.swift
@@ -20,12 +20,24 @@ class FavoritesListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        PersistenceManager.retrieveFavorites { (result) in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                print(error.rawValue)
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
+    
+    
+    // MARK: - Configuration Methods
     
     // MARK: - Navigation
 
