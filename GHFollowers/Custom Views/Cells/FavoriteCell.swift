9e6264efbf3f8cf3890a457a972b870fca3a6d0e
Øystein Berget <oystein.berget@gmail.com>
2022-02-11 19:19:24 +0100

    Fixed error in FaviriteCell layout where constrain was referred for self...

--- GHFollowers/Custom Views/Cells/FavoriteCell.swift
+++ GHFollowers/Custom Views/Cells/FavoriteCell.swift
@@ -37,14 +37,14 @@ class FavoriteCell: UITableViewCell {
         let padding: CGFloat    = 12
         
         NSLayoutConstraint.activate([
-            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            avatarImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
             avatarImageView.heightAnchor.constraint(equalToConstant: 60),
             avatarImageView.widthAnchor.constraint(equalToConstant: 60),
             
-            usernameLable.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            usernameLable.centerYAnchor.constraint(equalTo: centerYAnchor),
             usernameLable.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
-            usernameLable.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            usernameLable.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
             usernameLable.heightAnchor.constraint(equalToConstant: 40)
         ])
     }
